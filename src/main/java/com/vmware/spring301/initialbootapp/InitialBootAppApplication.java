d0ae32126584dd28a52093dde4a0a3b219e22421
sranjan001 <sranjan@pivotal.io>
2021-03-04 10:20:38 +0800

    Initial check in

---
+++ src/main/java/com/vmware/spring301/initialbootapp/InitialBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.spring301.initialbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmware/spring301/initialbootapp/InitialBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.spring301.initialbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
